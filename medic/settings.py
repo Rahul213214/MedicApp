2033415246a4c2416f89494ab31716c58cfdf5c3
Rahul Dalvi <rahuldalvi246@gmail.com>
2023-10-24 10:21:05 +0530

    3nd commit

--- medic/settings.py
+++ medic/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-j5nq(xehms0^29@dt0na(v$xgi!ew+*d*r42co(uoejkra3k9+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
